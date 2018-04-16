8d02adcbb612f318e7feb37fc8a3596f1e347047
Sergey Besedin <kr3ssh@gmail.com>
2018-04-16 17:43:03 +0300

    Add nologin shell for dante user

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Create users
 if [ "$DANTE_USER" ]; then
-  adduser -D -H -s /bin/sh $DANTE_USER
+  adduser -D -H -s /sbin/nologin $DANTE_USER
   echo $DANTE_USER:$DANTE_PASSWORD | chpasswd
 fi
 
